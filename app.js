1bfaa15abcb4e7ff9794d9d3c542c849bcae64c4
andreiutz7z <andrei.achim.6669@gmail.com>
2022-11-08 13:16:33 +0200

    commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
+const app = express();
 port = 3000 || process.env.PORT;
 
 app.listen(port, () => {
